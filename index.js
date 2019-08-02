d2b58a2baf0bcfa4342d7c4213de000416ce9305
Jonathan Traverso <jtraverso7@gmail.com>
2019-08-01 23:49:18 -0400

    Custom logger function implemented.

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const server = express();
 const postRoute = require("./posts/postRouter");
 const userRoute = require("./users/userRouter");
 
+// ==== LOGGER MIDDLEWARE .GET('ORIGIN) DOES NOT WORK SO I REMOVED IT BUT IT STILL LOGS THE OTHER INFO ====
+// ==== .GET('ORIGIN') MAY WORK WHEN THE CLIENT ISNT POSTMAN ====
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} request to ${req.url}.`
+  );
+  next();
+}
+
 server.use(express.json());
+server.use(logger);
 server.use("/api/users", userRoute);
 server.use("/api/posts", postRoute);
 
